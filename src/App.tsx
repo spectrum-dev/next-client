447235557e395dd8f11ee0936f6f43976c416fb4
Rahul V Brahmal <rahul.brahmal@me.com>
2021-07-17 19:58:30 +0100

    (feature): Added React Router to project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,26 @@
-import * as React from 'react';
+import {
+  Router,
+  Redirect,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+
+import history from 'app/history';
+
+// Screens
+import Login from 'containers/Authentication/Login';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit
-            {' '}
-            <Code fontSize="xl">src/App.tsx</Code>
-            {' '}
-            and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+    <Router history={history}>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Redirect from="/" to="/login" />
+      </Switch>
+    </Router>
   </ChakraProvider>
 );
---
+++ src/containers/Authentication/Login/index.tsx
@@ -0,0 +1,5 @@
+const Login = () => (
+  <h1> Debug Page </h1>
+);
+
+export default Login;
