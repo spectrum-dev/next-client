d1017aa49ecf39310f0b57560624d7af6cabc791
Rahul V Brahmal <rahul.brahmal@me.com>
2021-07-18 09:29:25 +0100

    (feature): Added useAuth hook to AccountSwitcher.

--- src/containers/Dashboard/Navbar/AccountSwitcher.tsx
+++ src/containers/Dashboard/Navbar/AccountSwitcher.tsx
@@ -9,22 +9,28 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 
+import useAuth from 'containers/Authentication/Login/useAuth';
+
 import { AccountSwitcherButton } from './AccountSwitcherButton';
 
-export const AccountSwitcher = () => (
-  <Menu>
-    <AccountSwitcherButton />
-    <MenuList shadow="lg" py="4" color={useColorModeValue('gray.600', 'gray.200')} px="3">
-      <Text fontWeight="medium" mb="2">
-        placeholder@imbue.dev
-      </Text>
-      <MenuOptionGroup defaultValue="chakra-ui">
-        <MenuItemOption value="chakra-ui" fontWeight="semibold" rounded="md">
-          Spectrum Alpha
-        </MenuItemOption>
-      </MenuOptionGroup>
-      <MenuDivider />
-      <MenuItem rounded="md">Logout</MenuItem>
-    </MenuList>
-  </Menu>
-);
+export const AccountSwitcher = () => {
+  const { signOut } = useAuth();
+
+  return (
+    <Menu>
+      <AccountSwitcherButton />
+      <MenuList shadow="lg" py="4" color={useColorModeValue('gray.600', 'gray.200')} px="3">
+        <Text fontWeight="medium" mb="2">
+          placeholder@imbue.dev
+        </Text>
+        <MenuOptionGroup defaultValue="chakra-ui">
+          <MenuItemOption value="chakra-ui" fontWeight="semibold" rounded="md">
+            Spectrum Alpha
+          </MenuItemOption>
+        </MenuOptionGroup>
+        <MenuDivider />
+        <MenuItem rounded="md" onClick={() => signOut()}>Logout</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
