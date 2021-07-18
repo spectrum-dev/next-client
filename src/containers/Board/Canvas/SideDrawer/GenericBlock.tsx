14f58bca16bc6fb49deb7f4a06417430e8ddbf0b
Rahul V Brahmal <rahul.brahmal@me.com>
2021-07-18 14:31:08 +0100

    (lint): Removed errant console.logo in onDrag.

--- src/containers/Board/Canvas/SideDrawer/GenericBlock.tsx
+++ src/containers/Board/Canvas/SideDrawer/GenericBlock.tsx
@@ -7,7 +7,7 @@ const GenericBlock = (
   { onDrag, blockName, blockType }:
   { onDrag: Function, blockName: String, blockType: String },
 ) => (
-  <Box width="80%" height="80px" borderRadius="25px" border="1px solid #1a202c;" background="linear-gradient(0deg, #151a23 0% 40%, #1a202c 40% 100%)" textAlign="center" margin="10px 0px 10px 0px" draggable onDragStart={() => { onDrag(); console.log('Drag Start'); }}>
+  <Box width="80%" height="80px" borderRadius="25px" border="1px solid #1a202c;" background="linear-gradient(0deg, #151a23 0% 40%, #1a202c 40% 100%)" textAlign="center" margin="10px 0px 10px 0px" draggable onDragStart={() => { onDrag(); }}>
     <Text color="white" marginTop="12px">
       { blockName }
     </Text>
