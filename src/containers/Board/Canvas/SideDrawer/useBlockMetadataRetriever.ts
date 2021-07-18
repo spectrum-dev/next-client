b38671dfbb18b7ca2a888f66dd64a975f8856ce7
Rahul Brahmal <rahul.brahmal@me.com>
2021-07-18 14:36:55 +0100

    Rahul/implement block metadata functionality (#17)
    * (feature): Integrated uesBlockMetadataRetriever hook.
    
    * (lint): Removed errant console.logo in onDrag.
    
    * (fix): Fixed key error with divider in SideDrawer.

---
+++ src/containers/Board/Canvas/SideDrawer/useBlockMetadataRetriever.ts
@@ -0,0 +1,50 @@
+import { useState, useEffect, useCallback } from 'react';
+import { useToast } from '@chakra-ui/react';
+
+import fetcher from 'app/fetcher';
+
+const GET_ALL_METADATA_RESPONSE_500 = 'Error retrieving block metadata';
+
+interface State {
+  isLoading: boolean;
+  hasError: boolean;
+  blockMetadata: Record<any, any> | undefined;
+}
+
+export default function useBlockMetadataRetriever() {
+  const [state, setState] = useState<State>({
+    isLoading: true,
+    hasError: false,
+    blockMetadata: undefined,
+  });
+  const toast = useToast();
+
+  const fetchData = useCallback(async () => {
+    try {
+      const metadataResponse = await fetcher('/orchestration/metadata');
+
+      if (metadataResponse.status === 200) {
+        setState((element) => ({
+          ...element,
+          blockMetadata: metadataResponse?.data?.response,
+        }));
+      } else {
+        throw new Error(GET_ALL_METADATA_RESPONSE_500);
+      }
+    } catch (e) {
+      toast({
+        title: GET_ALL_METADATA_RESPONSE_500,
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return state;
+}
