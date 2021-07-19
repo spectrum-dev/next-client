1e9a243c3e97ed53a30923bf5944ca7b0d45e72b
Rahul V Brahmal <rahul.brahmal@me.com>
2021-07-19 18:29:07 +0100

    (fix): Updated typing for onSaveStrategy in controls.

--- src/containers/Board/Canvas/Controls/index.tsx
+++ src/containers/Board/Canvas/Controls/index.tsx
@@ -6,7 +6,7 @@ import { FaRobot } from 'react-icons/fa';
 
 const Controls = (
   { onViewBlocks, onSaveStrategy }:
-  { onViewBlocks: () => void, onSaveStrategy: any },
+  { onViewBlocks: () => void, onSaveStrategy: Function },
 ) => (
   <Box zIndex={10} position="absolute" top="90%" right="45%" background="#1a202c" borderRadius="30px" padding="0px 10px 0px 10px" justify="center">
     <IconButton aria-label="Save strategy" icon={<RiSaveFill color="white" size={20} />} background="#1a202c" onClick={() => onSaveStrategy()} />
