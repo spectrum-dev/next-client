203b3f6a0a4819a4cef3841fb789d5bc8fd8cb4e
Rahul V Brahmal <rahul.brahmal@me.com>
2021-07-22 11:51:29 +0100

    (fix): Updated UI of sidebar.

--- src/containers/Board/Canvas/SideDrawer/GenericBlock.tsx
+++ src/containers/Board/Canvas/SideDrawer/GenericBlock.tsx
@@ -7,12 +7,12 @@ const GenericBlock = (
   { onDrag, blockName, blockType }:
   { onDrag: React.DragEventHandler<HTMLDivElement>, blockName: String, blockType: String },
 ) => (
-  <Box width="80%" height="80px" borderRadius="25px" border="1px solid #1a202c;" background="linear-gradient(0deg, #151a23 0% 40%, #1a202c 40% 100%)" textAlign="center" margin="10px 0px 10px 0px" draggable onDragStart={onDrag}>
+  <Box width="73%" height="90px" borderRadius="20px" border="1px solid #1a202c;" background="linear-gradient(0deg, #151a23 0% 40%, #1a202c 40% 100%)" textAlign="center" margin="20px 0px 30px 0px" draggable onDragStart={onDrag}>
     <Text color="white" marginTop="12px">
       { blockName }
     </Text>
 
-    <Text color="white" marginTop="12px" fontWeight="bold">
+    <Text color="white" marginTop="21px" fontWeight="bold">
       { blockType }
     </Text>
   </Box>
