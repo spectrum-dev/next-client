3cb2a2efef6a154bbad889e095bc56df7452558a
Rahul V Brahmal <rahul.brahmal@me.com>
2021-07-27 20:12:27 +0100

    (feature): ADded a confirm step for deleting strategies.

---
+++ src/containers/Dashboard/StrategyList/DeleteStrategy/DeleteStrategyModal.tsx
@@ -0,0 +1,43 @@
+import { useRef } from 'react';
+
+import {
+  Button, Modal, ModalOverlay, ModalContent, ModalHeader,
+  ModalCloseButton, ModalFooter,
+} from '@chakra-ui/react';
+
+const DeleteStrategyModal = (
+  {
+    isOpen, onClose, strategyId, onDelete,
+  }:
+  { isOpen: boolean; onClose: () => void, strategyId: String, onDelete: Function },
+) => {
+  const initialRef = useRef();
+  const finalRef = useRef();
+
+  return (
+    <>
+      <Modal
+        // @ts-ignore
+        initialFocusRef={initialRef}
+        // @ts-ignore
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Confirm strategy deletion</ModalHeader>
+          <ModalCloseButton />
+          <ModalFooter>
+            <Button colorScheme="red" mr={3} onClick={() => onDelete(strategyId)}>
+              Delete
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export { DeleteStrategyModal };
--- src/containers/Dashboard/StrategyList/Description.tsx
+++ src/containers/Dashboard/StrategyList/Description.tsx
@@ -1,9 +1,11 @@
 import {
-  Box, HStack, IconButton, useColorModeValue as mode,
+  Box, HStack, IconButton, useColorModeValue as mode, useDisclosure,
 } from '@chakra-ui/react';
 
 import { useHistory } from 'react-router';
 import { HiCalendar, HiPencilAlt, HiTrash } from 'react-icons/hi';
+
+import { DeleteStrategyModal } from './DeleteStrategy/DeleteStrategyModal';
 
 interface DescriptionProps {
   title: string
@@ -18,6 +20,12 @@ export const Description = (props: DescriptionProps) => {
     title, children, creationDate, strategyId, onDelete,
   } = props;
 
+  const {
+    isOpen: isDeleteStrategyOpen,
+    onOpen: onDeleteStrategyOpen,
+    onClose: onDeleteStrategyClose,
+  } = useDisclosure();
+
   const history = useHistory();
 
   const handleStrategyEdit = () => {
@@ -25,26 +33,34 @@ export const Description = (props: DescriptionProps) => {
   };
 
   return (
-    <Box position="relative">
-      <Box fontWeight="bold" maxW="xl">
-        {title}
+    <>
+      <Box position="relative">
+        <Box fontWeight="bold" maxW="xl">
+          {title}
+        </Box>
+        <HStack fontSize="sm" fontWeight="medium" color={mode('gray.500', 'white')} mt="1">
+          <Box as={HiCalendar} fontSize="md" color="gray.400" />
+          <span>{creationDate}</span>
+        </HStack>
+        <Box mt="3" maxW="xl" color={mode('gray.600', 'gray.200')}>
+          {children}
+        </Box>
+        <HStack
+          position={{ sm: 'absolute' }}
+          top={{ sm: '0' }}
+          insetEnd={{ sm: '0' }}
+          mt={{ base: '4', sm: '0' }}
+        >
+          <IconButton aria-label="Edit" icon={<HiPencilAlt />} rounded="full" size="sm" onClick={() => handleStrategyEdit()} />
+          <IconButton aria-label="Delete" icon={<HiTrash />} rounded="full" size="sm" onClick={() => onDeleteStrategyOpen()} />
+        </HStack>
       </Box>
-      <HStack fontSize="sm" fontWeight="medium" color={mode('gray.500', 'white')} mt="1">
-        <Box as={HiCalendar} fontSize="md" color="gray.400" />
-        <span>{creationDate}</span>
-      </HStack>
-      <Box mt="3" maxW="xl" color={mode('gray.600', 'gray.200')}>
-        {children}
-      </Box>
-      <HStack
-        position={{ sm: 'absolute' }}
-        top={{ sm: '0' }}
-        insetEnd={{ sm: '0' }}
-        mt={{ base: '4', sm: '0' }}
-      >
-        <IconButton aria-label="Edit" icon={<HiPencilAlt />} rounded="full" size="sm" onClick={() => handleStrategyEdit()} />
-        <IconButton aria-label="Delete" icon={<HiTrash />} rounded="full" size="sm" onClick={() => onDelete(strategyId)} />
-      </HStack>
-    </Box>
+      <DeleteStrategyModal
+        isOpen={isDeleteStrategyOpen}
+        onClose={onDeleteStrategyClose}
+        onDelete={onDelete}
+        strategyId={strategyId}
+      />
+    </>
   );
 };
