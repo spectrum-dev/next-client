d14fbd6f5fb23ce70963f4e36554186d2ea188b2
Rahul V Brahmal <rahul.brahmal@me.com>
2021-07-27 20:23:48 +0100

    (feature): Added a check to see if elements is nothing, to show a different tooltip message.

--- src/containers/Board/Canvas/Controls/index.tsx
+++ src/containers/Board/Canvas/Controls/index.tsx
@@ -8,19 +8,32 @@ const Controls = (
   {
     onViewBlocks,
     onSaveStrategy,
+    hasStartedStrategy,
     isStrategyValid,
     onRunStrategy,
   }:
   {
     onViewBlocks: () => void;
     onSaveStrategy: Function;
+    hasStartedStrategy: boolean;
     isStrategyValid: boolean;
     onRunStrategy: Function;
   },
 ) => (
   <Box zIndex={10} position="absolute" top="90%" right="45%" background="#1a202c" borderRadius="30px" padding="0px 10px 0px 10px" justify="center">
     <IconButton aria-label="Save strategy" icon={<RiSaveFill color="white" size={20} />} background="#1a202c" onClick={() => onSaveStrategy()} />
-    <Tooltip hasArrow label="Please resolve any errors you have before running the strategy" bg="#1a202c" color="white" isOpen={!isStrategyValid} textAlign="center">
+    <Tooltip
+      hasArrow
+      label={
+      hasStartedStrategy
+        ? 'Please resolve any errors you have before running the strategy'
+        : 'Drag blocks from the sidebar to assemble your strategy'
+      }
+      bg="#1a202c"
+      color="white"
+      isOpen={!isStrategyValid}
+      textAlign="center"
+    >
       <IconButton aria-label="Run strategy" icon={<BsPlayFill color="white" size={20} />} background="#1a202c" disabled={!isStrategyValid} onClick={() => onRunStrategy()} />
     </Tooltip>
     <IconButton aria-label="View blocks" icon={<FaRobot color="white" size={20} />} background="#1a202c" onClick={() => onViewBlocks()} />
