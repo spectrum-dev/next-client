fa4a5369390a520a22b25ffb6fa4e0a5808f1612
Rahul V Brahmal <rahul.brahmal@me.com>
2021-08-07 21:45:57 +0100

    (fix): Adjusted onDrop ID for node so its RESULTS-label .

--- src/containers/Board/Canvas/ResultsDrawer/useResultsOnDrop.ts
+++ src/containers/Board/Canvas/ResultsDrawer/useResultsOnDrop.ts
@@ -1,18 +1,6 @@
-import { useState } from 'react';
-
 export default function useResultsOnDrop() {
-  const [id, setId] = useState<number>(1);
-
-  const generateID = () => {
-    setId(id + 1);
-    return id.toString();
-  };
-
   const onDrop = async (event: any, reactFlowInstance: any, setElements: any) => {
-    // Extracts data from headers
     const label = event.dataTransfer.getData('application/reactflow-data-label');
-    // const value = event.dataTransfer.getData('application/reactflow-data-value');
-    // const type = event.dataTransfer.getData('application/reactflow-data-type');
 
     const flowBlockType = event.dataTransfer.getData('application/reactflow-flow-block-type');
 
@@ -22,7 +10,7 @@ export default function useResultsOnDrop() {
 
     const position = reactFlowInstance.project({ x: event.clientX, y: event.clientY - 40 });
     const newNode: any = {
-      id: `RESULTS-${generateID()}`,
+      id: `RESULTS-${label}`,
       type: 'resultBlock',
       position,
       data: { label },
