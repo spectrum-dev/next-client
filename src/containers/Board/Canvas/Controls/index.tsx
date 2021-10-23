fdd86e58c0d8906806f1b415bd8b7985d6a37fc0
Rahul Brahmal <rahul.brahmal@me.com>
2021-10-23 18:05:01 +0100

    (feature): Hide results dashboard for screener. (#201)

--- src/containers/Board/Canvas/Controls/index.tsx
+++ src/containers/Board/Canvas/Controls/index.tsx
@@ -1,8 +1,11 @@
+import { useContext } from 'react';
 import { Box, IconButton, Tooltip } from '@chakra-ui/react';
 
 import { RiSaveFill } from 'react-icons/ri';
 import { BsPlayFill, BsGraphUp } from 'react-icons/bs';
 import { FaRobot } from 'react-icons/fa';
+
+import BoardContext from 'app/contexts/board';
 
 const Controls = (
   {
@@ -25,27 +28,35 @@ const Controls = (
     onRunStrategy: Function;
     isRunStrategyLoading: boolean;
   },
-) => (
-  <Box zIndex={10} position="absolute" top="90%" background="#1a202c" borderRadius="30px" padding="0px 10px 0px 10px">
-    <IconButton aria-label="Save strategy" icon={<RiSaveFill color="white" size={20} />} background="#1a202c" onClick={() => onSaveStrategy()} />
-    <Tooltip
-      disabled={false}
-      hasArrow
-      label={
-      hasStartedStrategy
-        ? 'Please resolve any errors you have before running the strategy'
-        : 'Drag blocks from the sidebar to assemble your strategy'
+) => {
+  const { strategyType } = useContext(BoardContext);
+
+  return (
+    <Box zIndex={10} position="absolute" top="90%" background="#1a202c" borderRadius="30px" padding="0px 10px 0px 10px">
+      <IconButton aria-label="Save strategy" icon={<RiSaveFill color="white" size={20} />} background="#1a202c" onClick={() => onSaveStrategy()} />
+      <Tooltip
+        disabled={false}
+        hasArrow
+        label={
+        hasStartedStrategy
+          ? 'Please resolve any errors you have before running the strategy'
+          : 'Drag blocks from the sidebar to assemble your strategy'
+        }
+        bg="#1a202c"
+        color="white"
+        isOpen={!isStrategyValid}
+        textAlign="center"
+      >
+        <IconButton aria-label="Run strategy" icon={<BsPlayFill color="white" size={20} />} background="#1a202c" disabled={!isStrategyValid || isRunStrategyLoading} onClick={() => onRunStrategy()} />
+      </Tooltip>
+      <IconButton aria-label="View blocks" icon={<FaRobot color="white" size={20} />} background="#1a202c" onClick={() => onViewBlocks()} />
+      {
+        strategyType === 'BACKTEST' && (
+          <IconButton aria-label="View results" icon={<BsGraphUp color="white" size={20} />} background="#1a202c" disabled={!showResults} onClick={() => onResultsPane()} />
+        )
       }
-      bg="#1a202c"
-      color="white"
-      isOpen={!isStrategyValid}
-      textAlign="center"
-    >
-      <IconButton aria-label="Run strategy" icon={<BsPlayFill color="white" size={20} />} background="#1a202c" disabled={!isStrategyValid || isRunStrategyLoading} onClick={() => onRunStrategy()} />
-    </Tooltip>
-    <IconButton aria-label="View blocks" icon={<FaRobot color="white" size={20} />} background="#1a202c" onClick={() => onViewBlocks()} />
-    <IconButton aria-label="View results" icon={<BsGraphUp color="white" size={20} />} background="#1a202c" disabled={!showResults} onClick={() => onResultsPane()} />
-  </Box>
-);
+    </Box>
+  );
+};
 
 export default Controls;
