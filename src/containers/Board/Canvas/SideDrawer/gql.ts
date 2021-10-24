6d786b72d3883cc19a3bfa0834de10e64aee1a63
Rahul V Brahmal <rahul.brahmal@me.com>
2021-10-24 17:13:08 +0100

    (feature): Updated the ALL_METADATA query to pass in strategyType.

--- src/containers/Board/Canvas/SideDrawer/gql.ts
+++ src/containers/Board/Canvas/SideDrawer/gql.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_ALL_METADATA = gql`
-    query ALL_METADATA {
-        allMetadata
+    query ALL_METADATA($strategyType: StrategyType!) {
+        allMetadata(strategyType: $strategyType)
     }
 `;
