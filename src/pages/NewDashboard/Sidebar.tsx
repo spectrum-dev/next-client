23d4b7beb7813b73024a40d83a97d7d06bac91a2
Rahul Brahmal <rahul.brahmal@me.com>
2021-12-06 19:50:19 +0000

    (feature): Added support for route navigation.

--- src/pages/NewDashboard/Sidebar.tsx
+++ src/pages/NewDashboard/Sidebar.tsx
@@ -1,16 +1,26 @@
 import { Box, Flex, Tooltip } from '@chakra-ui/react';
-import React from 'react';
 import { RiDashboard2Line, RiFlaskLine, RiLoginCircleLine, RiSettings2Line, RiBookLine } from 'react-icons/ri';
+import { useHistory } from 'react-router';
 
 import { ReactComponent as Logo } from './black-logo.svg';
 
-const SidebarElement = ({ label, children }: { label: string, children: React.ReactNode }) => (
-    <Tooltip hasArrow label={label} placement='right' offset={[0, 37]}>
-        <Box marginTop="1.5rem" marginBottom="1.5rem">
-            {children}
-        </Box>
-    </Tooltip>
-);
+const SidebarElement = ({ label, route, children }: { label: string, route?: string, children: React.ReactNode }) => {
+  const history = useHistory();
+  
+  const onClick = () => {
+    if (route) {
+      history.push(route);
+    }
+  };
+  
+  return (
+        <Tooltip hasArrow label={label} placement='right' offset={[0, 37]}>
+            <Box marginTop="1.5rem" marginBottom="1.5rem" onClick={onClick}>
+                {children}
+            </Box>
+        </Tooltip>
+  );
+};
 
 const Divider = () => (
     <Box borderBottomWidth="1px" borderBottomStyle="solid" borderBottomColor="#e7e9f3" minWidth="100%" />
@@ -23,23 +33,23 @@ const Sidebar = () => (
         </Flex>
 
         <Flex justifyContent="center" alignItems="center" flexDirection="column" justify="space-between" marginTop="1.5rem">
-            <SidebarElement label="Dashboard">
+            <SidebarElement label="Dashboard" route="/newDashboard">
                 <RiDashboard2Line size={28} color="#9D9DBF" />
             </SidebarElement>
             
-            <SidebarElement label="Strategies">
+            <SidebarElement label="Strategies" route="/strategies">
                 <RiFlaskLine size={28} color="#9D9DBF"/>
             </SidebarElement>
         </Flex>
 
         <Flex justifyContent="center" alignItems="center" bottom="0px" position="absolute" width="100%" borderTopWidth="1px" borderRightStyle="solid" borderRightColor="#e7e9f3" flexDirection="column">
-            <SidebarElement label="Documentation">
+            <SidebarElement label="Documentation" route="/documentation">
                 <RiBookLine size={28} color="#9D9DBF" />
             </SidebarElement>
             
             <Divider />
 
-            <SidebarElement label="Settings">
+            <SidebarElement label="Settings" route="/documentation">
                 <RiSettings2Line size={28} color="#9D9DBF" />
             </SidebarElement>
 
