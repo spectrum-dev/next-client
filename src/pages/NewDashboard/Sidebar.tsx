2b6e86d8708801378e1400fe7a3803026306da6c
Rahul Brahmal <rahul.brahmal@me.com>
2021-12-06 19:39:11 +0000

    (feature): Condensed the component down and cleaned up the structure.

--- src/pages/NewDashboard/Sidebar.tsx
+++ src/pages/NewDashboard/Sidebar.tsx
@@ -1,7 +1,16 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Tooltip } from '@chakra-ui/react';
+import React from 'react';
 import { RiDashboard2Line, RiFlaskLine, RiLoginCircleLine, RiSettings2Line, RiBookLine } from 'react-icons/ri';
 
 import { ReactComponent as Logo } from './black-logo.svg';
+
+const SidebarElement = ({ label, children }: { label: string, children: React.ReactNode }) => (
+    <Tooltip hasArrow label={label} placement='right' offset={[0, 37]}>
+        <Box marginTop="1.5rem" marginBottom="1.5rem">
+            {children}
+        </Box>
+    </Tooltip>
+);
 
 const Divider = () => (
     <Box borderBottomWidth="1px" borderBottomStyle="solid" borderBottomColor="#e7e9f3" minWidth="100%" />
@@ -14,31 +23,31 @@ const Sidebar = () => (
         </Flex>
 
         <Flex justifyContent="center" alignItems="center" flexDirection="column" justify="space-between" marginTop="1.5rem">
-            <Box marginTop="1.5rem" marginBottom="1.5rem">
+            <SidebarElement label="Dashboard">
                 <RiDashboard2Line size={28} color="#9D9DBF" />
-            </Box>
-
-            <Box marginTop="1.5rem" marginBottom="1.5rem">
+            </SidebarElement>
+            
+            <SidebarElement label="Strategies">
                 <RiFlaskLine size={28} color="#9D9DBF"/>
-            </Box>
+            </SidebarElement>
         </Flex>
 
         <Flex justifyContent="center" alignItems="center" bottom="0px" position="absolute" width="100%" borderTopWidth="1px" borderRightStyle="solid" borderRightColor="#e7e9f3" flexDirection="column">
-            <Box marginTop="1.5rem" marginBottom="1.5rem">
+            <SidebarElement label="Documentation">
                 <RiBookLine size={28} color="#9D9DBF" />
-            </Box>
+            </SidebarElement>
             
             <Divider />
 
-            <Box marginTop="1.5rem" marginBottom="1.5rem">
+            <SidebarElement label="Settings">
                 <RiSettings2Line size={28} color="#9D9DBF" />
-            </Box>
+            </SidebarElement>
 
             <Divider />
 
-            <Box marginTop="1.5rem" marginBottom="1.5rem">
+            <SidebarElement label="Log Out">
                 <RiLoginCircleLine size={28} color="#9D9DBF" />
-            </Box>
+            </SidebarElement>
         </Flex>
     </Box>
 );
