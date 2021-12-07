04a06b6f2e1a485f02334ae35f277990b0fa20a0
Rahul Brahmal <rahul.brahmal@me.com>
2021-12-07 22:58:31 +0000

    Support for Creating Strategies (#282)
    * (feature): Implemented support for the create strategy modal.
    
    * (feature): Added a right pane.

--- src/pages/NewDashboard/Dashboard/index.tsx
+++ src/pages/NewDashboard/Dashboard/index.tsx
@@ -1,31 +1,36 @@
+import { useContext } from 'react';
 import { Box, Flex, Text, Button } from '@chakra-ui/react';
 import { RiPlug2Line } from 'react-icons/ri';
 
 import InfoPane from 'components/InfoPane';
 
-const NoActiveStrategies = () => (
-    <Flex borderColor="#E6E9F3" borderStyle="dashed" borderWidth="1px" flexDirection="column" align="center">
-        <Box margin="5rem 0rem 3rem 0rem" align="center">
-            <Box marginBottom="1rem">
-                <RiPlug2Line fontSize={41} />
-            </Box>
-            <Text fontSize="xl" fontWeight="500" marginBottom="0.5rem"> Create or start a strategy </Text>
-            <Text fontSize="sm" fontWeight="300"> Running strategies will be shown to you here once they are started </Text>
-        </Box>
+import DashboardContext from 'app/contexts/dashboard';
 
-        <Flex marginBottom="5rem">
-            <Button colorScheme='blue' size='md' marginRight="1rem" disabled> Create a strategy </Button>
-            <Button colorScheme='blue' size='md' disabled> Start your strategy </Button>
-        </Flex>
-    </Flex>
-);
+const NoActiveStrategies = () => {
+  const { onCreateStrategyOpen } = useContext(DashboardContext);
 
-const Dashboard = () => {
   return (
-        <InfoPane title="Active Strategies" description="List of your running bots with real or virtual money">
-            <NoActiveStrategies />
-        </InfoPane>
+        <Flex borderColor="#E6E9F3" borderStyle="dashed" borderWidth="1px" flexDirection="column" align="center">
+            <Box margin="5rem 0rem 3rem 0rem" align="center">
+                <Box marginBottom="1rem">
+                    <RiPlug2Line fontSize={41} />
+                </Box>
+                <Text fontSize="xl" fontWeight="500" marginBottom="0.5rem"> Create or start a strategy </Text>
+                <Text fontSize="sm" fontWeight="300"> Running strategies will be shown to you here once they are started </Text>
+            </Box>
+    
+            <Flex marginBottom="5rem">
+                <Button colorScheme='blue' size='md' marginRight="1rem" onClick={() => onCreateStrategyOpen()}> Create a strategy </Button>
+                <Button colorScheme='blue' size='md' disabled> Start your strategy </Button>
+            </Flex>
+        </Flex>
   );
 };
+
+const Dashboard = () =>  (
+    <InfoPane title="Active Strategies" description="List of your running bots with real or virtual money">
+        <NoActiveStrategies />
+    </InfoPane>
+);
 
 export { Dashboard };
--- src/pages/NewDashboard/Strategies/index.tsx
+++ src/pages/NewDashboard/Strategies/index.tsx
@@ -1,12 +1,24 @@
+import { useContext } from 'react';
+
+import { Button } from '@chakra-ui/react';
+
 import InfoPane from 'components/InfoPane';
+import DashboardContext from 'app/contexts/dashboard';
+
 import { DataTable } from './DataTable';
 
 
 const Strategies = () => {
+  const { onCreateStrategyOpen } = useContext(DashboardContext);
+
+  const CreateNewStrategyButton = () => (
+    <Button colorScheme='blue' size='md' onClick={() => onCreateStrategyOpen()}> Create new strategy </Button>
+  );
+
   return (
-        <InfoPane title="Strategies" description="In this section you can create new strategies or edit your already created bots.">
-            <DataTable />
-        </InfoPane>
+    <InfoPane title="Strategies" description="In this section you can create new strategies or edit your already created bots." rightIcon={<CreateNewStrategyButton />}>
+        <DataTable />
+    </InfoPane>
   );
 };
 
