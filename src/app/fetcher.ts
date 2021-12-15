c20e5c88f22bdfe7d89ce3b57593a2740803c204
Rahul Brahmal <rahul.brahmal@me.com>
2021-12-15 11:37:10 +0800

    (revert): Reverted the URL's for graphQL and fetcher.

--- src/app/fetcher.ts
+++ src/app/fetcher.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // eslint-disable-next-line prefer-destructuring
-const REACT_APP_API_BASE_URL = 'https://api.imbue.dev';
+const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const fetcher = axios.create({
   baseURL: REACT_APP_API_BASE_URL,
