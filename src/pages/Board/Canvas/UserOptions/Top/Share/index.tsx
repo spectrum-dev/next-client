82eef851a54b5449293719f406c9dd635c515e53
Rahul Brahmal <rahul.brahmal@me.com>
2021-12-16 08:57:53 +0800

    (fix): Adjusted spacing.

--- src/pages/Board/Canvas/UserOptions/Top/Share/index.tsx
+++ src/pages/Board/Canvas/UserOptions/Top/Share/index.tsx
@@ -12,12 +12,20 @@ const Share = ({ top }: { top: string }) => (
         height="3rem"
     >
         <Box>
-            <Button backgroundColor="#4262FF" textColor="white" height="2rem" fontSize="0.8rem" borderRadius="3px"> Share </Button>
-        </Box>
+             <Button
+                 backgroundColor="#4262FF"
+                 textColor="white"
+                 height="2rem"
+                 fontSize="0.8rem"
+                 borderRadius="3px"
+             >
+                 Share
+             </Button>
+         </Box>
 
-        <Box marginLeft="1rem">
-            <RiQuestionLine fontSize={28} />
-        </Box>
+         <Box marginLeft="1rem" _hover={{ color: '#4262FF' }}>
+             <RiQuestionLine fontSize={28} />
+         </Box>
     </OptionsBox>
 );
 
