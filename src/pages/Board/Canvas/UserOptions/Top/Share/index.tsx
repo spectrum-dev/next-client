ce808f054ace54d1e4ce207a1a506ce5713c8c66
Rahul Brahmal <rahul.brahmal@me.com>
2021-12-19 14:24:46 +0800

    (feature): Re-wired up the sharing modal to the new board. (#305)

--- src/pages/Board/Canvas/UserOptions/Top/Share/index.tsx
+++ src/pages/Board/Canvas/UserOptions/Top/Share/index.tsx
@@ -1,32 +1,39 @@
+import { useContext } from 'react';
 import { Box, Button } from '@chakra-ui/react';
 
 import { RiQuestionLine } from 'react-icons/ri';
 
 import { OptionsBox } from '../../OptionsBox';
+import CanvasContext from 'app/contexts/canvas';
 
-const Share = ({ top }: { top: string }) => (
-    <OptionsBox
-        top={top}
-        right="4.25rem"
-        width="8rem"
-        height="3rem"
-    >
-        <Box>
-             <Button
-                 backgroundColor="#4262FF"
-                 textColor="white"
-                 height="2rem"
-                 fontSize="0.8rem"
-                 borderRadius="3px"
-             >
-                 Share
-             </Button>
-         </Box>
+const Share = ({ top }: { top: string }) => {
+  const { onSharingOpen } = useContext(CanvasContext);
 
-         <Box marginLeft="1rem" _hover={{ color: '#4262FF' }}>
-             <RiQuestionLine fontSize={28} />
-         </Box>
-    </OptionsBox>
-);
+  return (
+        <OptionsBox
+            top={top}
+            right="4.25rem"
+            width="8rem"
+            height="3rem"
+        >
+            <Box>
+                 <Button
+                     backgroundColor="#4262FF"
+                     textColor="white"
+                     height="2rem"
+                     fontSize="0.8rem"
+                     borderRadius="3px"
+                     onClick={onSharingOpen}
+                 >
+                     Share
+                 </Button>
+             </Box>
+    
+             <Box marginLeft="1rem" _hover={{ color: '#4262FF' }}>
+                 <RiQuestionLine fontSize={28} />
+             </Box>
+        </OptionsBox>
+  );
+};
 
 export default Share;
